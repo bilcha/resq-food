8dc9b7584fc04d2c6018e644dcd3eda91bed9ecb
Olena Ivanova <ivanova.olenk@gmail.com>
2025-08-06 14:09:53 -0700

    add vercel URL to backend CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   app.enableCors({
     origin:
       process.env.NODE_ENV === 'production'
-        ? ['https://resqfood.vercel.app', 'https://www.resqfood.com']
+        ? ['https://resq-food-frontend.vercel.app']
         : ['http://localhost:5173', 'http://localhost:3000'],
     credentials: true,
   });
